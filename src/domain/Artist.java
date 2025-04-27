51b281e71f9e0ce1deca9a12946c66427257944b
Ambrosov Danyil <166511703+F1xaze11@users.noreply.github.com>
2025-04-27 14:33:20 +0300

    Update Artist.java

--- src/domain/Artist.java
+++ src/domain/Artist.java
@@ -1,22 +1,44 @@
 package domain;
 
+/**
+ * Клас, що представляє художника, який є співробітником і має певні навички.
+ */
 public class Artist extends Employee {
 
+    /**
+     * Конструктор з повною ініціалізацією.
+     * @param skiils Масив навичок.
+     * @param name Ім'я співробітника.
+     * @param jobTitle Посада співробітника.
+     * @param level Рівень співробітника.
+     * @param dept Відділ співробітника.
+     */
     public Artist(String[] skiils, String name, String jobTitle, int level, String dept) {
         super(name, jobTitle, level, dept);
         this.skiils = skiils;
     }
 
+    /**
+     * Конструктор з навичками.
+     * @param skiils Масив навичок.
+     */
     public Artist(String[] skiils) {
         super();
         this.skiils = skiils;
     }
-    
+
+    /**
+     * Конструктор за замовчуванням.
+     */
     public Artist() {
         super();
         this.skiils = new String[10];
     }
 
+    /**
+     * Повертає рядкове представлення об'єкта Artist.
+     * @return Інформація про художника.
+     */
     @Override
     public String toString() {
         return super.toString()+"\nSkills: "+getSkills();
@@ -24,19 +46,31 @@ public class Artist extends Employee {
 
     private String[] skiils;
 
+    /**
+     * Повертає навички у вигляді одного рядка.
+     * @return Список навичок через кому.
+     */
     public String getSkills() {
         String s = "";
         for (String e : skiils) {
             s = s + e + ", ";
         }
-       s=s.substring(0, s.length() - 2);
+        s = s.substring(0, s.length() - 2);
         return s;
     }
 
+    /**
+     * Встановлює масив навичок.
+     * @param skills Масив навичок.
+     */
     public void setSkills(String[] skills) {
-        this.skiils=skills;
+        this.skiils = skills;
     }
 
+    /**
+     * Повертає масив навичок.
+     * @return Масив навичок.
+     */
     public String[] getSkillsLSist() {
         return skiils;
     }
